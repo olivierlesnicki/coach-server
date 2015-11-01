769989558df699a7e7390d1299d35521161713da
Olivier Lesnicki <olivier.lesnicki@gmail.com>
2015-11-01 16:24:46 +0000

    Attempt to suppress error

--- index.js
+++ index.js
@@ -14,54 +14,54 @@ var ref = new Firebase(FIREBASE_REF);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-ref
-  .child('incoming-messages')
-  .orderByChild('timestamp')
-  .startAt(new Date().getTime())
-  .on('child_added', function(snapshot) {
-    var message = snapshot.val();
-
-    request({
-      method: 'POST',
-      uri: 'https://slack.com/api/channels.create',
-      body: {
-        token: SLACK_WEB_API_TOKEN,
-        name: message.text,
-      },
-      json: true,
-    }).then(() => {
-      return Promise.resolve();
-    }, () => {
-      return new Promise((resolve, reject) => {
-        console.log(arguments);
-      });
-    }).then(() => {
-
-      // Post the message as the user on slack
-      request({
-        method: 'POST',
-        uri: SLACK_INCOMING_WEBHOOK_COACH_URL,
-        body: {
-          username: message.from,
-          text: message.text,
-          channel: '#' + message.from,
-        },
-        json: true,
-      });
-
-      // Store the message in the thread
-      ref
-        .child('threads')
-        .child(message.from)
-        .push({
-          text: message.text,
-          timestamp: Firebase.ServerValue.TIMESTAMP,
-          coach: false,
-        });
-
-    });
-
-  });
+// ref
+//   .child('incoming-messages')
+//   .orderByChild('timestamp')
+//   .startAt(new Date().getTime())
+//   .on('child_added', function(snapshot) {
+//     var message = snapshot.val();
+//
+//     request({
+//       method: 'POST',
+//       uri: 'https://slack.com/api/channels.create',
+//       body: {
+//         token: SLACK_WEB_API_TOKEN,
+//         name: message.text,
+//       },
+//       json: true,
+//     }).then(() => {
+//       return Promise.resolve();
+//     }, () => {
+//       return new Promise((resolve, reject) => {
+//         console.log(arguments);
+//       });
+//     }).then(() => {
+//
+//       // Post the message as the user on slack
+//       request({
+//         method: 'POST',
+//         uri: SLACK_INCOMING_WEBHOOK_COACH_URL,
+//         body: {
+//           username: message.from,
+//           text: message.text,
+//           channel: '#' + message.from,
+//         },
+//         json: true,
+//       });
+//
+//       // Store the message in the thread
+//       ref
+//         .child('threads')
+//         .child(message.from)
+//         .push({
+//           text: message.text,
+//           timestamp: Firebase.ServerValue.TIMESTAMP,
+//           coach: false,
+//         });
+//
+//     });
+//
+//   });
 
 app.use('/slack/command/reply', require('./lib/routes/slack/command/reply'));
 app.use('/login', require('./lib/routes/login'));
